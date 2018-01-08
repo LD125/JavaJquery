7191265bd6d5993f18f335e91f2c890a883919c3
LD125 <djadel.lounis@gmail.com>
2018-01-08 17:08:12 +0100

    04/Angular 08/01
    Angular + form contact

---
+++ 04-Angular/ContactApp/src/app/app.component.ts
@@ -0,0 +1,71 @@
+//--Importation de "Component" via @angulare/core
+//-- Pour déclarer une classe comme commposant de notre application, on importe "Component" via @angular/core
+import { Component } from '@angular/core';
+/**
+ * Création d'une interface Contact
+ * cela me permet de définir la forme/structure de mes objets contact.d
+ */
+interface Contact {
+  id     : number;
+  prenom : string;
+  nom    : string;
+  email?  : string;
+}
+interface ContactArray {
+  [index: number] : Contact;
+}
+/**
+ * @Component est ce qu'on appelle un décorateur.
+ * Il va nous permettre de définir 3 paramètres essentiels à notre application
+ */
+@Component({
+  /**
+   * Le sélécteur (selector)  déternimine la manière dont le composant sera affiché dans un template
+   * On ecrira dans notre HTML: <app-root></app-root>
+   * Vous devez OBLIGATOIREMENT avoir la labalise d'ouverture et la balise de fermeture.
+   */
+  selector: 'app-root',
+  /**
+   * "templateUrl" ou "template est la partie visible du composant, c'est ce qui s'affiche à l'écran
+   * lorsque lle composant est utlisé"
+   */
+  templateUrl: './app.component.html',
+  /**
+   * La déclaration des style avec "styleUrls" ou "styles"
+   */
+  styleUrls: ['./app.component.css']
+})
+/**
+ * La classe contient les données du composant, mais aussi son comportement.
+ * Dans le contexte MVVM, notre classe correspond au ViewModel.
+ */
+export class AppComponent {
+ //--- déclaration d'une variable title
+ title = 'ContactApp';
+  contact:Contact = {
+    id      : 1,
+    prenom  : "Lounis",
+    nom     : "Djadel",
+    email   : "lounis.djadel@gmail.com",
+  }
+  //--- Tableau contenant collection de contacts
+  contacts:ContactArray = [
+    {id: 1, prenom:"Lounis", nom:"DJADEL"},
+    {id: 2, prenom:"Francis", nom:"ALBERT"},
+    {id: 3, prenom:"Jean", nom:"DIDIER"},
+  ]
+/**
+ * 
+ */
+  getNomComplet = (UnContact:Contact) => {
+    return UnContact.prenom + ' ' + UnContact.nom;
+  }
+
+//-- Choix de mon utilisateur actif contactActif;
+contactActif;
+//-- Permet de définir un contact actif @param {contact} UnContact
+choisirUnContact = (UnContact:Contact) => {
+  this.contactActif = UnContact;
+  console.log(this.contactActif);
+}
+}
